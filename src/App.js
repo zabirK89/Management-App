2d9929ec167f346d96ac15dc7cce0bea6a4003bc
zabirK89 <zabirk89@gmail.com>
2022-02-14 14:43:14 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./style.css";
 import Header from"./Components/Header"
+import LoginPage from "./Components/LoginPage"
 import Sidebar from "./Components/Sidebar"
 export default function App() {
   return (
     <div>
       
-
-    <Sidebar/>
+<LoginPage/>
+    {/* <Sidebar/> */}
     </div>
   );
 }
---
+++ src/Components/LoginPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Box from '@mui/material/Box';
+export default function header(){
+  return (
+    <div>
+        <Box
+      sx={{
+        width: 300,
+        height:"100px",
+        padding:"200px",
+        marginLeft:"28%",
+        marginTop:"12%",
+        backgroundColor: 'cyan',
+         
+      }}
+    >
+      </Box>
+      </div>
+  )
+}
