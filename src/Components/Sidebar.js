73901da7e98c7caa368767d1410847b750082f95
zabirK89 <zabirk89@gmail.com>
2022-02-15 15:00:26 +0530

    1 file modified

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -31,8 +31,8 @@ export default function ClippedDrawer() {
         <Toolbar>
           <Typography variant="h6" noWrap component="div">
             <div className="header-item">
-          <Avatar style={{marginRight:"25px",color:"whitesmoke"}} /> 
-          <InputIcon  style={{color:"whitesmoke"}}/>
+          <Avatar style={{marginRight:"25px",color:"167, 184, 171"}} /> 
+          <InputIcon  style={{color:"167, 184, 171"}}/>
           </div>
           </Typography>
         
