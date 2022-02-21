e8d505d37424d1c14ce0f67f3092a5bbc9d16456
zabirK89 <zabirk89@gmail.com>
2022-02-21 11:09:11 +0530

    1 file modified

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -5,83 +5,96 @@ import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Divider from '@mui/material/Divider';
-import { Link } from 'react-router-dom';
-import {useState} from "react"
-import "../style.css";
-export default function header(){
-  const [email,setemail]=useState("")
-  const [password ,setpassword]=useState("")
-  const [entry,setentry]=useState([]);
- const changeemail=(e)=>{
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from "react";
+export default function header({ setUser }) {
+  const [email, setemail] = useState("")
+  const [password, setpassword] = useState("")
+  const [entry, setentry] = useState([]);
+  const navigate = useNavigate();
+  const changeemail = (e) => {
     setemail(e.target.value)
-   
-  }
-  const changepassword=(e)=>{
-    setpassword(e.target.value)
-    
-  }
 
-  const login=(e)=>{
-    e.preventDefault()
-    const newentry={email:email,password:password}
-    setentry([...entry,newentry])
-    console.log(entry)
   }
+  const changepassword = (e) => {
+    setpassword(e.target.value)
+
+  }
+async function login(e){
+  e.preventDefault()
+  // console.log(email,password);
+  let result=await fetch("https://lms-dev.webileapps.io/api/users/admin/login",{
+    method:"POST",
+    header:{
+        "Content-Type":"application/json",
+        "Accept":"application/json"
+    },
+    body:JSON.stringify({ "email": "webileadmin@webileapps.com",
+    "password": "webile@123"})
+  })
+  result=await result.json();
+  console.log(result)
+  console.log("clicked")
+  localStorage.setItem("user-info",JSON.stringify(result))
+
+
+
+}
   return (
     <React.Fragment>
-    <CssBaseline />
-    <Container maxWidth="sm">
-      <Typography component="div" style={{  boxShadow: "10px 8px 10px  10px #aaaaaa", backgroundColor: 'white', height: '400px' ,marginTop:"36%" ,width:"600px",height:"500px",marginTop:"130px",borderRadius:"10px"}} >
-      <div classname="logincont" style={{padding:"30px"}}>
-        <h2>Admin Login</h2>
-        <br/>
-        <TextField
-          id="outlined-full-width"
-          label="Email Address
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Typography component="div" style={{ boxShadow: "10px 8px 10px  10px #aaaaaa", backgroundColor: 'white', height: '400px', marginTop: "36%", width: "600px", height: "500px", marginTop: "130px", borderRadius: "10px" }} >
+          <div classname="logincont" style={{ padding: "30px" }}>
+            <h2>Admin Login</h2>
+            <br />
+            <TextField
+              id="outlined-full-width"
+              label="Email Address
           "
-          style={{ margin: 8 ,width:"550px"}}
-          placeholder="Email Address
+              style={{ margin: 8, width: "550px" }}
+              placeholder="Email Address
           "
-        
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-          type="email"
-          onChange={changeemail}
-        />
-        <br/>
-           <TextField
-          id="outlined-full-width"
-          label="Password "
-          style={{ margin: 8 ,width:"550px"}}
-          placeholder="Password"
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-          type="password"
-          onChange={changepassword}
-        />
-        <br/>
-        <br/>
-        <Link to="/admin" style={{ textDecoration: 'none' }}>
-   <Button variant="contained" color="primary" style={{width:"550px"}} onClick={login}> 
-  Login
-</Button>
-</Link>
-<br/>
-      </div>
-      <div style={{marginLeft:"60px"}}>This login is only for ADMIN!</div>
-      <br/>
-      <Divider/>
-      <div style={{marginLeft:"40px"}}>Create a new password</div>
-      <div style={{marginLeft:"40px"}}>Forgot password</div>
-      </Typography>
-    </Container>
-  </React.Fragment>
+
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              type="email"
+              onChange={changeemail}
+            />
+            <br />
+            <TextField
+              id="outlined-full-width"
+              label="Password "
+              style={{ margin: 8, width: "550px" }}
+              placeholder="Password"
+              margin="normal"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              type="password"
+              onChange={changepassword}
+            />
+            <br />
+            <br />
+            <Link to="/admin" style={{ textDecoration: 'none' }}>
+              <Button variant="contained" color="primary" style={{ width: "550px" }} onClick={login}>
+                Login
+              </Button>
+            </Link>
+            <br />
+          </div>
+          <div style={{ marginLeft: "60px" }}>This login is only for ADMIN!</div>
+          <br />
+          <Divider />
+          <div style={{ marginLeft: "40px" }}>Create a new password</div>
+          <div style={{ marginLeft: "40px" }}>Forgot password</div>
+        </Typography>
+      </Container>
+    </React.Fragment>
   )
 }
