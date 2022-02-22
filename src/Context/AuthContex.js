deaf7920b94cec08a47e3ac89be0ba8a83eac70c
zabirK89 <zabirk89@gmail.com>
2022-02-22 12:28:23 +0530

    1 file modified

--- src/Context/AuthContex.js
+++ src/Context/AuthContex.js
@@ -1,102 +1,71 @@
-import React from "react"
-import { createContext, useState, useEffect } from "react";
-const Authcontext = createContext()
+import React from 'react';
+import { createContext, useState, useEffect } from 'react';
+const Authcontext = createContext();
 import { useNavigate } from 'react-router-dom';
-export default Authcontext
-
-
+export default Authcontext;
 
 export const AuthProvider = ({ children }) => {
-
   let navigate = useNavigate();
   let login = async (e) => {
-    e.preventDefault()
-    console.log("result")
+    e.preventDefault();
+    console.log('result');
     console.log(email, password);
     try {
-const proxy="https://cors-anywhere.herokuapp.com/"
-const mainurl="https://lms-dev.webileapps.io"
-      let result = await fetch(`https://react-gnjfdf.stackblitz.io/api/users/admin/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          "Accept": "application/json"
-        },
-        body: JSON.stringify({
-          'email': email,
-          'password': password
-        })
-      })
+      const proxy = 'https://cors-anywhere.herokuapp.com/';
+      const mainurl = 'https://lms-dev.webileapps.io';
+      let result = await fetch(
+        `https://react-gnjfdf.stackblitz.io/api/users/admin/login`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        }
+      );
       result = await result.json();
 
-   
-        setAuthToken(result.token)
-  
-   
-      console.log("result:", result)
+      setAuthToken(result.token);
+
+      console.log('result:', result);
       console.log(result.token);
-      localStorage.setItem("authToken", JSON.stringify(result))
-      setUser("a")
-      navigate("/admin")
+      localStorage.setItem('authToken', JSON.stringify(result));
+      setUser('a');
+      navigate('/admin');
       // if (email === "webileadmin@webileapps.com" && password === "webile@123") {
       //   navigate('/admin');
       // } else {
       //   // alert("enter correct password")
       //   navigate("/")
       // }
-
-    }
-    catch (err) {
+    } catch (err) {
+      console.log(err);
       alert(err.message);
     }
-
-
-  }
-
+  };
 
   const changeemail = (e) => {
-
-    setemail(e.target.value)
-
-
-  }
+    setemail(e.target.value);
+  };
   const changepassword = (e) => {
-    setpassword(e.target.value)
-
-  }
+    setpassword(e.target.value);
+  };
   const [user, setUser] = useState(null);
-  const [email, setemail] = useState("")
-  const [password, setpassword] = useState("")
-  const [authToken,setAuthToken]=useState(null)
+  const [email, setemail] = useState('');
+  const [password, setpassword] = useState('');
+  const [authToken, setAuthToken] = useState(null);
   let contextData = {
     login: login,
     changeemail: changeemail,
     changepassword: changepassword,
-    user:setUser,
-    authToken:authToken
-   
-  }
+    user: setUser,
+    authToken: authToken,
+  };
   return (
-    <Authcontext.Provider value={contextData}>
-      {children}
-    </Authcontext.Provider>
-  )
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <Authcontext.Provider value={contextData}>{children}</Authcontext.Provider>
+  );
+};
