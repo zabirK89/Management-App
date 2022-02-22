690b0d1c2ae2c48036cf8e3c173eb5023acd81f1
zabirK89 <zabirk89@gmail.com>
2022-02-22 09:58:14 +0530

    1 file modified, 1 added, 4 deleted

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import React from 'react';
+// import React from 'react';
+import React,{ useContext } from 'react';
 import './style.css';
-import Header from './Components/Header';
 import LoginPage from './Components/LoginPage';
 import Sidebar from './Components/Sidebar';
-import Tickets from "./Components/Tickets"
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Holiday from './Components/Holiday';
-import Assets from "./Components/Assets"
+// import Tickets from "./Components/Tickets"
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+// import Holiday from './Components/Holiday';
+// import Assets from "./Components/Assets";
+import Authcontext, { AuthProvider } from './Context/AuthContex';
+
 export default function App() {
+  // const info=useGetAllPostQuery()
+  // console.log(info)
+  // const [user, setUser] = useState(null);
+  let user = useContext(Authcontext);
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/holiday" element={<Holiday />} />
-        <Route path="/admin" element={<Sidebar />}></Route>
-        <Route path="/tickets" element={<Tickets />} />
-        <Route path="/assets" element={<Assets />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<LoginPage></LoginPage>} />
+
+          {/* <Route path="/admin" element={<ProtectRoute user={user} ><Sidebar /></ProtectRoute>}/>   */}
+
+          <Route path="/admin" element={<Sidebar />} />
+
+          {/* <Route path="/admin" element={<ProtectRoute><Sidebar /></ProtectRoute>}/> */}
+          {/* <Route path="/tickets" element={ <ProtectRoute user={user} ><Tickets /></ProtectRoute>} />
+        <Route path="/tickets" element={  <Tickets />} />
+        <Route path="/assets" element={<ProtectRoute user={user} ><Assets /></ProtectRoute>} />
+        <Route path="/assets" element={<Assets />} /> */}
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
+
+// const ProtectRoute = ({ user, children }) => {
+
+//   return user == null ? <Navigate to="/"></Navigate> : children;
+// }
