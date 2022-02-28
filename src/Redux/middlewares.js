ea16a97bf702fce8b4fdbde4b6988f442b1ae2dc
zabirK89 <zabirk89@gmail.com>
2022-02-28 15:25:51 +0530

    2 files modified

--- src/Redux/middlewares.js
+++ src/Redux/middlewares.js
@@ -1,9 +1,91 @@
 
 
+// const baseurl = `https://lms-dev.webileapps.io/api`
+// export const Fetching_Api = 'Fetching_Api';
+
+// // const response=await fetch("https://react-gnjfdf.stackblitz.io/api/api/users/admin/login")
+
+
+// const DEFAULT_HEADERS = {
+//     Accept: "application/json",
+//     "Content-Type": "application/json",
+// }
+
+// async function invokeAPI({ endpoint, config, headerContent, contentType }) {
+
+//     const headers = DEFAULT_HEADERS;
+//     const updatedConfig = {
+//         ...config,
+//         headers: { ...headers, ...headerContent },
+//     }
+
+//     // console.log("config---->?", updatedConfig);
+//     const url = `${baseurl}${endpoint}`;
+
+//     // console.log("endpoints----->", endpoint)
+//     const response = await fetch(url, updatedConfig)
+//     // console.log(response)
+  
+//     let result;
+//     if (typeof contentType === 'undefined') {
+//         result = await response.json();
+//         console.log("Result ---->", result.token);
+//         console.log(result);
+//         localStorage.setItem('token',result.token);
+//         return { data: result };
+//     } else if (contentType === 'application/pdf') {
+//         result = await response.blob();
+//         console.log("Result ---->", result);
+//         return { data: result };
+//     }
+// }
+
+
+// export const middleWareDispatch = (store) => (next) => async (action) => {
+//     if (typeof action[Fetching_Api] === 'undefined') {
+//         return next(action);
+//     }
+//     const { silent = false } = action[Fetching_Api];
+//     try {
+//         const { url, body, method = 'POST', json, contentType } = action[Fetching_Api];
+//         console.log("body = ", body);
+//         const { token } = store.getState();
+
+//         const headerContent = {
+//             ...(token ? { Authorization: `Bearer ${token}` } : {}),
+//             ...DEFAULT_HEADERS,
+//         };
+//         const payload = {
+//             endpoint: url,
+
+//             headerContent,
+//             config: {
+//                 method,
+//                 body: JSON.stringify(body),
+//             },
+//             json,
+//             contentType,
+//         };
+//         const { data } = await invokeAPI(payload);
+//         // console.log(url)
+//         return data;
+//     } catch (error) {
+//         console.log(error);
+
+//     }
+// };
+
+
+
+
+
+
+import { LOGOUT_SUCCESS } from "./Type/logintype"
+
 const baseurl = `https://lms-dev.webileapps.io/api`
 export const Fetching_Api = 'Fetching_Api';
 
-// const response=await fetch("https://react-gnjfdf.stackblitz.io/api/api/users/admin/login")
+// const response=await fetch("https://lms-dev.webileapps.io/api/users/admin/login")
 
 
 const DEFAULT_HEADERS = {
@@ -35,7 +117,7 @@ async function invokeAPI({ endpoint, config, headerContent, contentType }) {
         return { data: result };
     } else if (contentType === 'application/pdf') {
         result = await response.blob();
-        console.log("Result ---->", result);
+        console.log("Result ---->", result);2
         return { data: result };
     }
 }
@@ -57,7 +139,7 @@ export const middleWareDispatch = (store) => (next) => async (action) => {
         };
         const payload = {
             endpoint: url,
-
+            
             headerContent,
             config: {
                 method,
@@ -67,7 +149,7 @@ export const middleWareDispatch = (store) => (next) => async (action) => {
             contentType,
         };
         const { data } = await invokeAPI(payload);
-        // console.log(url)
+        console.log(url)
         return data;
     } catch (error) {
         console.log(error);
