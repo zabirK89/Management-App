886697b5d8a4a59700e725526002afe75a2c423b
zabirK89 <zabirk89@gmail.com>
2022-02-28 10:07:03 +0530

    1 file modified

--- src/Redux/middlewares.js
+++ src/Redux/middlewares.js
@@ -1,6 +1,6 @@
 import { LOGOUT_SUCCESS } from "./Type/logintype"
 
-const baseurl = `http://localhost:3000/api`
+const baseurl = `https://react-gnjfdf.stackblitz.io/api`
 export const CALL_API = 'CALL_API';
 
 // const response=await fetch("https://lms-dev.webileapps.io/api/users/admin/login")
