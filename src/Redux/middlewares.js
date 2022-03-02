dd1ad1d362b8139d29be8672e144cc5c479eb3ba
zabirK89 <zabirk89@gmail.com>
2022-03-02 15:59:10 +0530

    1 file modified

--- src/Redux/middlewares.js
+++ src/Redux/middlewares.js
@@ -1,73 +1,72 @@
-const baseurl = `https://lms-dev.webileapps.io/api`;
+
+
+const baseurl = `https://lms-dev.webileapps.io/api`
 export const Fetching_Api = 'Fetching_Api';
 const DEFAULT_HEADERS = {
-  Accept: 'application/json',
-  'Content-Type': 'application/json',
-};
+    Accept: "application/json",
+    "Content-Type": "application/json",
+}
 
 async function invokeAPI({ endpoint, config, headerContent, contentType }) {
-  const headers = DEFAULT_HEADERS;
-  const updatedConfig = {
-    ...config,
-    headers: { ...headers, ...headerContent },
-  };
 
-  // console.log("config---->?", updatedConfig);
-  const url = `${baseurl}${endpoint}`;
+    const headers = DEFAULT_HEADERS;
+    const updatedConfig = {
+        ...config,
+        headers: { ...headers, ...headerContent },
+    }
 
-  // console.log("endpoints----->", endpoint)
-  const response = await fetch(url, updatedConfig);
-  // console.log(response)
+    // console.log("config---->?", updatedConfig);
+    const url = `${baseurl}${endpoint}`;
 
-  let result;
-  if (typeof contentType === 'undefined') {
-    result = await response.json();
-    console.log('Result ---->', result.token);
-    console.log(result);
-    localStorage.setItem('authReducer', result);
-    return { data: result };
-  } else if (contentType === 'application/pdf') {
-    result = await response.blob();
-    console.log('Result ---->', result);
-    localStorage.setItem('authReducer', result);
-    return { data: result };
-  }
+    // console.log("endpoints----->", endpoint)
+    const response = await fetch(url, updatedConfig)
+    // console.log(response)
+  
+    let result;
+    if (typeof contentType === 'undefined') {
+        result = await response.json();
+        console.log("Result ---->", result.token);
+        console.log(result);
+        localStorage.setItem('authReducer',result);
+        return { data: result };
+    } else if (contentType === 'application/pdf') {
+        result = await response.blob();
+        console.log("Result ---->", result);
+        localStorage.setItem('authReducer',result);
+        return { data: result };
+    }
 }
 
 export const middleWareDispatch = (store) => (next) => async (action) => {
-  if (typeof action[Fetching_Api] === 'undefined') {
-    return next(action);
-  }
-  const { silent = false } = action[Fetching_Api];
-  try {
-    const {
-      url,
-      body,
-      method = 'POST',
-      json,
-      contentType,
-    } = action[Fetching_Api];
-    console.log('body = ', body);
-    const { token: token } = store.getState();
-    console.log('token', token);
-    const headerContent = {
-      ...(token ? { Authorization: `Bearer ${token.token}` } : {}),
-      ...DEFAULT_HEADERS,
-    };
-    const payload = {
-      endpoint: url,
-      headerContent,
-      config: {
-        method,
-        body: JSON.stringify(body),
-      },
-      json,
-      contentType,
-    };
-    const { data } = await invokeAPI(payload);
-    // console.log(url)
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+    if (typeof action[Fetching_Api] === 'undefined') {
+        return next(action);
+    }
+    const { silent = false } = action[Fetching_Api];
+    try {
+        const { url, body, method = 'POST', json, contentType } = action[Fetching_Api];
+        console.log("body = ", body);
+        const { authReducer:token } = store.getState();
+
+
+        const headerContent = {
+            ...(token ? { Authorization: `Bearer ${token}` } : {}),
+            ...DEFAULT_HEADERS,
+        };
+        const payload = {
+            endpoint: url,
+            headerContent,
+            config: {
+                method,
+                body: JSON.stringify(body),
+            },
+            json,
+            contentType,
+        };
+        const { data } = await invokeAPI(payload);
+        // console.log(url)
+        return data;
+    } catch (error) {
+        console.log(error);
+
+    }
 };
