07f1afd5bc5a9dae194b988624739a45fc7f4d0e
zabirK89 <zabirk89@gmail.com>
2022-03-02 17:02:37 +0530

    1 file modified

--- src/Components/UserContent.js
+++ src/Components/UserContent.js
@@ -26,7 +26,7 @@ function createData(name, calories, fat, carbs, protein) {
 
 export default function UserContent() {
   const dispatch=useDispatch()
-const users=useSelector((state)=>state.users)
+// const users=useSelector((state)=>state.users)
 console.log("users=====>",users)
 useEffect(()=>{
 async function Users(){
