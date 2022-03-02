48ef1d8d3ecf85558b1c901305571893c062cccd
zabirK89 <zabirk89@gmail.com>
2022-03-02 17:03:39 +0530

    1 file modified

--- src/Redux/middlewares.js
+++ src/Redux/middlewares.js
@@ -26,11 +26,13 @@ async function invokeAPI({ endpoint, config, headerContent, contentType }) {
     if (typeof contentType === 'undefined') {
         result = await response.json();
         console.log("Result ---->", result.token);
+        console.log(result);
       
         return { data: result };
     } else if (contentType === 'application/pdf') {
         result = await response.blob();
         console.log("Result ---->", result);
+        // localStorage.setItem('authReducer',result);
         return { data: result };
     }
 }
