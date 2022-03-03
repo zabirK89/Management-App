f498be27c5647999d47787a5cb48efc90ff73522
zabirK89 <zabirk89@gmail.com>
2022-03-03 14:56:52 +0530

    1 file modified

--- src/Routes/ProtectedRoute.js
+++ src/Routes/ProtectedRoute.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Route } from 'react-router';
+import {Navigate} from "react-router-dom"
 
 function ProtectedRoute({ children, isLoggedIn }) {
   console.log('isLoggedIn = ', isLoggedIn);
-  return isLoggedIn === 'super_admin' ? children : <Redirect to="/" />;
+  return isLoggedIn === 'super_admin' ? children : <Navigate to="/" />;
 }
 export default ProtectedRoute;
