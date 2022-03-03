263d95d4f089624881a65660fad876a987a97d3a
zabirK89 <zabirk89@gmail.com>
2022-03-03 15:03:32 +0530

    2 files modified

--- src/Routes/ProtectedRoute.js
+++ src/Routes/ProtectedRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Redirect, Route } from 'react-router';
+
 import { Redirect } from 'react-router-dom';
 
 function ProtectedRoute({ children, isLoggedIn }) {
--- src/Routes/redirect.js
+++ src/Routes/redirect.js
@@ -1,5 +1,4 @@
 import { connect } from 'react-redux';
-import { Redirect, Route } from 'react-router';
 import { Redirect } from 'react-router-dom';
 
 function RedirectRoute({ isLoggedIn, children }) {
