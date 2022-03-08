f2e83b4acc9c3f9d09a61c2316a89fe4e2309c63
zabirK89 <zabirk89@gmail.com>
2022-03-08 17:02:44 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { useContext, useState } from 'react';
 import './style.css';
 import AppRoutes from "./Routes/index";
-import Form from "./Components/UserForm"
-import UserRoute from "./Components/UserRouter"
+
 export default function App() {
 
 
   return (
 <>
    <AppRoutes/> 
- 
-  {/* <UserRoute/> */}
+
    </>
 
   )
